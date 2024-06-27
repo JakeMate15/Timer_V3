185335d553636646e3aea57add091f7c5d1b6d55
JakeMate15 <erik.mofe51@gmail.com>
2024-06-27 17:04:14 -0600

    vista timer

--- Front/Timer/src/app/app.routes.ts
+++ Front/Timer/src/app/app.routes.ts
@@ -3,10 +3,27 @@ import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './servicios/auth.guard';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { TimerComponent } from './timer/timer.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { 
+        path: 'home', 
+        component: HomeComponent, 
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'acerca-de', component: AcercaDeComponent }
+        ]
+    },
+    { 
+        path: 'home', 
+        component: HomeComponent, 
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'timer', component: TimerComponent }
+        ]
+    }
 ];
